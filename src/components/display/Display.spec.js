ca3e87d7ba8bd0b37875bee35298f01ec17f408e
Andy Bettisworth <andy@accreu.com>
2019-06-19 17:41:55 -0500

    displays 'Locked' if the `locked` prop is `true` and 'Unlocked' if otherwise

--- src/components/display/Display.spec.js
+++ src/components/display/Display.spec.js
@@ -20,4 +20,14 @@ describe('Display.js', () => {
   test('it renders without errors', () => {
     render(<Display />)
   })
+
+  test('it displays "Locked" if the `locked` prop is `true`', () => {
+    const { getByText } = render(<Display locked={true} closed={false} />)
+    getByText('Locked')
+  })
+
+  test('it displays "Unlocked" if the `locked` prop is `false`', () => {
+    const { getByText } = render(<Display locked={false} closed={false} />)
+    getByText('Unlocked')
+  })
 })
