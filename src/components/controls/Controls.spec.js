0be634f4f5a25e7f8520f83407eb86fd3ea407c9
Andy Bettisworth <andy@accreu.com>
2019-06-19 17:36:04 -0500

    cannot be closed or opened if it is locked

--- src/components/controls/Controls.spec.js
+++ src/components/controls/Controls.spec.js
@@ -20,4 +20,16 @@ describe('Controls.js', () => {
   test('it renders without errors', () => {
     render(<Controls />)
   })
+
+  test('it cannot be closed if it is locked', () => {
+    const { getByText } = render(<Controls locked={true} closed={false} />)
+    const btn_close = getByText('Close Gate')
+    expect(btn_close.disabled).toBeTruthy()
+  })
+
+  test('it cannot be opened if it is locked', () => {
+    const { getByText } = render(<Controls locked={true} closed={true} />)
+    const btn_open = getByText('Open Gate')
+    expect(btn_open.disabled).toBeTruthy()
+  })
 })
